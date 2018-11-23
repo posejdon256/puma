3b676a3b723489ac70820c0f32c4a58e7617bff2
Anna Bujak <aniutka.pop@gmail.com>
2018-11-23 17:49:32 +0100

    fix

--- src/OpenGL/Shaders/lineVertShader.js
+++ src/OpenGL/Shaders/lineVertShader.js
@@ -5,7 +5,7 @@ uniform mat4 model;
 uniform mat4 projection;
 
 void main() {
-    gl_Position = vec4(vec4(position, 1.0) * projection * model);
+    gl_Position = vec4(projection * model * vec4(position, 1.0)s);
   }`;
   export function getVertexShaderLine() {
       return vertexShader;

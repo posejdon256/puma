b0d7a98190fc3ebbeadba555991726a56d2dfc8e
Anna Bujak <aniutka.pop@gmail.com>
2019-01-27 21:02:57 +0100

    add a3 a5 a1 update

--- src/canvas/Geometry/CountInverseKinematics.js
+++ src/canvas/Geometry/CountInverseKinematics.js
@@ -31,9 +31,8 @@ export function countInverseKinematics(alfa, beta, gamma, start, _animation, pre
 
     let q = getVectorLength(DiffPoints(p2, p1));
     let minusAlfa = false;
-    let minusAlfa2 = false;
     const _p2 = SumPoints(point, Multiply(normalize(_v), -getL2()));
-    if(prevQ && getVectorLength(DiffPoints(p2, prevQ)) > 2) {
+    if(prevQ && getVectorLength(DiffPoints(p2, prevQ)) > getVectorLength(DiffPoints(_p2, prevQ))) {
         p2 = _p2;
         q = getVectorLength(DiffPoints(p2, p1));
         minusAlfa = true;
@@ -46,40 +45,41 @@ export function countInverseKinematics(alfa, beta, gamma, start, _animation, pre
      const z5 = normalize(getZ({rx: alfa, ry: beta, rz: gamma, px: start.x, py: start.y, pz: start.z}));
      const x5 = normalize(getX({rx: alfa, ry: beta, rz: gamma, px: start.x, py: start.y, pz: start.z}));
 
-    let n = normalize(crossMultiply(p3, p1));
-     if(isNaN(n.x)) {
+    let n = crossMultiply(p3, p1);
+     if(getVectorLength(n) < 0.0001) {
          n.x = 0;
          n.y = 0;
          n.z = -1;
      }
-
-    let n2 = normalize(crossMultiply(normalize(DiffPoints(p1, p0)), normalize(DiffPoints(p2, p1))));
+     n = normalize(n);
+    let n2 = crossMultiply(normalize(DiffPoints(p1, p0)), normalize(DiffPoints(p2, p1)));
     let n3 = normalize(crossMultiply(DiffPoints(p1, p2), DiffPoints(p3, p2)));
     let n4 = normalize(crossMultiply(normalize(crossMultiply(p3, p1)), x5));
-    let n5 = normalize(crossMultiply(normalize(crossMultiply(p3, p2)), z5));
+   // addSphere(0, SumPoints(start, Multiply(z5, 10)));
 
-    const len2 = getVectorLength(SumPoints(n, n2));
+    const len2 = getVectorLength(SumPoints(n, normalize(n2)));
     const len3 = getVectorLength(SumPoints(n, n3));
-    const len4 = getVectorLength(SumPoints(n, n4));
-    const len5 = getVectorLength(SumPoints(n, n5));
+
 
     let a1 = Math.atan2(-p3.z, p3.x) + Math.PI;
     let a2 = Math.acos(scalarMultiply(normalize(DiffPoints(p1, p0)), normalize(DiffPoints(p2, p1))));
-    let a3 =  Math.acos(scalarMultiply(normalize(DiffPoints(p1, p2)), normalize(DiffPoints(p3, p2))));
+    //let a3 =  Math.acos(scalarMultiply(normalize(DiffPoints(p1, p2)), normalize(DiffPoints(p3, p2))));
     let a4 = Math.acos(scalarMultiply(normalize(crossMultiply(p3, p1)), x5));
-    let a5 = Math.acos(scalarMultiply(normalize(crossMultiply(p3, p2)), z5));
 
-    //a3 = -a3;
     a4 = minusAlfa ? -a4 : a4;
-    a2 = isNaN(len2) || Math.abs(len2) < 0.01 ? a2 : -a2;
-    a3 = !isNaN(len3) && Math.abs(len3) < 0.01 ? a3 : -a3;
-   // a4 = Math.abs(len2) < 0.01 ? a4 : -a4;
-    a5 = Math.abs(len5) < 0.01 ? -a5 : a5;
-    a2 = isNaN(n2.x) ? Math.PI / 2 : a2;
-    a3 = isNaN(n3.x) ? 0: a3;
+    a2 = !isNaN(len2) && Math.abs(len2) < 0.01 ? a2 : -a2;
+   // a3 = !isNaN(len3) && Math.abs(len3) < 0.01 ? a3 : -a3;
+    a2 = isNaN(n2.x) ? 0 : a2;
+  //  a3 = isNaN(n3.x) ? Math.PI : a3;
     a4 = isNaN(n4.x) ? Math.atan2(x5.z, x5.x) - Math.PI / 2 : a4;
-    a5 = isNaN(n5.x) ? Math.atan2(z5.z, z5.x) : a5;
 
+    const a3 = _angle(DiffPoints(p2, p1), DiffPoints(p3, p2), normalize(DiffPoints(p3, p1))) + Math.PI;
+    const a5 = -_angle(DiffPoints(p2, p3), z5, x5) + Math.PI / 2;
     return {a1: a1, a2: a2, a3: a3, a4: a4, a5: a5, q: q, p2: p2, crossPrev: n3};
 
+}
+function _angle(vec1, vec2, vec3) {
+    const _cross = crossMultiply(vec1, vec2);
+    const angle = Math.atan2(getVectorLength(_cross), scalarMultiply(vec1, vec2));
+    return scalarMultiply(_cross, vec3) < 0 ? -angle : angle;
 }
